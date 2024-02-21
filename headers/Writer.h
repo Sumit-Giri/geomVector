16a329dfb9e428b270d24302b4493ffd8d14f8cf
Sumit <sumit.giri@cctech.co.in>
2024-02-21 17:45:56 +0530

    updated

--- headers/Writer.h
+++ headers/Writer.h
@@ -8,8 +8,9 @@ class Writer
 public:
     Writer() = default;
     Writer(Vector3D ans);
+    Writer(Vector3D v1, Vector3D v2, Vector3D ans);
     ~Writer() = default;
 
-    Writer(Vector3D v1, Vector3D v2, Vector3D ans);
+   
 
 };
--- src/Writer.cpp
+++ src/Writer.cpp
@@ -1,15 +1,20 @@
 #include <fstream>
 #include "../headers/Writer.h"
 
+ofstream out("resources/output.txt");
 
-ofstream out ("output.txt");
- Writer::Writer(Vector3D v1 , Vector3D v2, Vector3D ans)
-    {
-        out << 0 << " " << 0 << " " << 0 << endl;
-        out << v1.x() << " " << v1.y() << " " << v1.z() << endl;
-        out << 0 << " " << 0 << " " << 0 << endl;
-        out << v2.x() << " " << v2.y() << " " << v2.z() << endl;
-        out << 0 << " " << 0 << " " << 0 << endl;
-        out << ans.x() << " " << ans.y() << " " << ans.z() << endl;
-    }
+Writer::Writer(Vector3D v1, Vector3D v2, Vector3D ans)
+{
+    out << 0 << " " << 0 << " " << 0 << endl;
+    out << v1.x() << " " << v1.y() << " " << v1.z() << endl;
+    out << 0 << " " << 0 << " " << 0 << endl;
+    out << v2.x() << " " << v2.y() << " " << v2.z() << endl;
+    out << 0 << " " << 0 << " " << 0 << endl;
+    out << ans.x() << " " << ans.y() << " " << ans.z() << endl;
+}
 
+Writer ::Writer(Vector3D ans)
+{
+    out << 0 << " " << 0 << " " << 0 << endl;
+    out << ans.x() << " " << ans.y() << " " << ans.z() << endl;
+}
